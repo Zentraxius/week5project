ca7509e1eb60ad885bf62ed4e654c78d6cda8519
Cody Fritz <clanalia55@gmail.com>
2020-06-26 13:52:57 -0700

    finish bulk of testing on businesslogic.test.js, will begin to finalize reverse file for negative testing

--- __tests__/businesslogic.test.js
+++ __tests__/businesslogic.test.js
@@ -31,13 +31,50 @@ describe("Age", () => {
 
   test("Should determine age on Venus, their expected lifespan, and their remaining lifespan. If lifespan is exceeded, indicate as such", () => {
     expect(reuseableAge.earthToVenus()).toEqual([16, 6200, 6184]);
+
+    expect("You are " + reuseableAge.earthToVenus()[0] + " years old on Venus!").toEqual("You are " + 16 + " years old on Venus!");  
+
+    expect("You are expected to live for a total of " + reuseableAge.earthToVenus()[1] + " years!").toEqual("You are expected to live for a total of " + reuseableAge.earthToVenus()[1] + " years!")
+
+  if (reuseableAge.earthToVenus()[2] < 0) {
+    expect("You have exceeded your life expectancy by " + reuseableAge.earthToVenus()[2] + " years!").toEqual("You have exceeded your life expectancy by " + reuseableAge.earthToVenus()[2] + " years!")
+  } else {
+
+    expect("You have " + reuseableAge.earthToVenus()[2] + " more years of life expectancy to enjoy!").toEqual("You have " + Math.abs(reuseableAge.earthToVenus()[2]) + " more years of life expectancy to enjoy!")
+  }
+
   });
 
   test("Should determine age on Jupiter, their expected lifespan, and their remaining lifespan. If lifespan is exceeded, indicate as such", () => {
     expect(reuseableAge.earthToJupiter()).toEqual([297, 118600, 118303]);
+
+    expect("You are " + reuseableAge.earthToJupiter()[0] + " years old on Jupiter!").toEqual("You are " + 297 + " years old on Jupiter!");  
+
+    expect("You are expected to live for a total of " + reuseableAge.earthToJupiter()[1] + " years!").toEqual("You are expected to live for a total of " + reuseableAge.earthToJupiter()[1] + " years!")
+
+  if (reuseableAge.earthToJupiter()[2] < 0) {
+    expect("You have exceeded your life expectancy by " + reuseableAge.earthToJupiter()[2] + " years!").toEqual("You have exceeded your life expectancy by " + reuseableAge.earthToJupiter()[2] + " years!")
+  } else {
+
+    expect("You have " + reuseableAge.earthToJupiter()[2] + " more years of life expectancy to enjoy!").toEqual("You have " + Math.abs(reuseableAge.earthToJupiter()[2]) + " more years of life expectancy to enjoy!")
+  }
+
+
   });
 
   test("Should determine age on Mercury, their expected lifespan, and their remaining lifespan. If lifespan is exceeded, indicate as such", () => {
     expect(reuseableAge.earthToMercury()).toEqual([6, 2400, 2394]);
+
+    expect("You are " + reuseableAge.earthToMercury()[0] + " years old on Mercury!").toEqual("You are " + 6 + " years old on Mercury!");  
+
+    expect("You are expected to live for a total of " + reuseableAge.earthToMercury()[1] + " years!").toEqual("You are expected to live for a total of " + reuseableAge.earthToMercury()[1] + " years!")
+
+  if (reuseableAge.earthToMercury()[2] < 0) {
+    expect("You have exceeded your life expectancy by " + reuseableAge.earthToMercury()[2] + " years!").toEqual("You have exceeded your life expectancy by " + reuseableAge.earthToMercury()[2] + " years!")
+  } else {
+
+    expect("You have " + reuseableAge.earthToMercury()[2] + " more years of life expectancy to enjoy!").toEqual("You have " + Math.abs(reuseableAge.earthToMercury()[2]) + " more years of life expectancy to enjoy!")
+  }
+
   });
 });
