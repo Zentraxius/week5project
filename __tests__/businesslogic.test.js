29d5c544bc6b3cec8c6fc10b4cf0719dd38ad68f
Cody Fritz <clanalia55@gmail.com>
2020-06-26 09:38:24 -0700

    initial test failed, beginning next stage of RGR

--- __tests__/businesslogic.test.js
+++ __tests__/businesslogic.test.js
@@ -1,1 +1,11 @@
 // Use import to bring stuff over and test
+import { Age } from './../src/businesslogic.js';
+import {TestScheduler} from 'jest';
+
+describe('Age', () => {
+let reuseableAge = new Age(25, 79)
+test('should determine age on earth', () => {
+  expect(reuseableAge.age).toEqual(1);
+  expect(reuseableAge.expectancy).toEqual(2);
+})
+})
