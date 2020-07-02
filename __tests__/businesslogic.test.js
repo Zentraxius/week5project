0d6811528c6c796f8c8ea494a3eb955834a2b2b3
Cody Fritz <clanalia55@gmail.com>
2020-07-02 14:44:04 -0700

    update working code to proper levels, all tests returned green, project complete mk2

--- __tests__/businesslogic.test.js
+++ __tests__/businesslogic.test.js
@@ -2,7 +2,7 @@ import { Age } from "./../src/businesslogic.js";
 
 describe("Age", () => {
   let reuseableAgeYoung = new Age(25, 10000);
-  let reuseableAgeOld = new Age(25000, 10000)
+  let reuseableAgeOld = new Age(25000, 10000);
 
   beforeEach(() => {
     reuseableAgeYoung = new Age(25, 10000);
@@ -13,7 +13,7 @@ describe("Age", () => {
     expect(reuseableAgeYoung.age).toEqual(25);
     expect(reuseableAgeYoung.expectancy).toEqual(10000);
     expect(reuseableAgeOld.age).toEqual(25000);
-    expect(reuseableAgeOld.expectancy).toEqual(10000)
+    expect(reuseableAgeOld.expectancy).toEqual(10000);
   });
 
   test("Should determine age, expectancy, and difference between them on Mars", () => {
@@ -25,42 +25,48 @@ describe("Age", () => {
     reuseableAgeOld.earthToMars();
     expect(reuseableAgeOld.age).toEqual(13298);
     expect(reuseableAgeOld.expectancy).toEqual(5319);
-    expect(reuseableAgeOld.difference).toEqual("You have exceeded your lifespan by 7979 years!");
+    expect(reuseableAgeOld.difference).toEqual(
+      "You have exceeded your lifespan by 7979 years!"
+    );
   });
 
-  // test("Should determine age, expectancy, and difference between them on Venus", () => {
-  //   reuseableAgeYoung.earthToVenus();
-  //   expect(reuseableAgeYoung.age).toEqual(40);
-  //   expect(reuseableAgeYoung.expectancy).toEqual(16129);
-  //   expect(reuseableAgeYoung.difference).toEqual(16089);
-  //   //
-  //   reuseableAgeOld.earthToMars();
-  //   expect(reuseableAgeOld.age).toEqual(13298);
-  //   expect(reuseableAgeOld.expectancy).toEqual(5319);
-  //   expect(reuseableAgeOld.difference).toEqual(-7979);
-  // });
-
-  // test("Should determine age, expectancy, and difference between them on Jupiter", () => {
-  //   reuseableAgeYoung.earthToJupiter();
-  //   expect(reuseableAgeYoung.age).toEqual(2);
-  //   expect(reuseableAgeYoung.expectancy).toEqual(843);
-  //   expect(reuseableAgeYoung.difference).toEqual(841);
-  //   //
-  //   reuseableAgeOld.earthToMars();
-  //   expect(reuseableAgeOld.age).toEqual(13298);
-  //   expect(reuseableAgeOld.expectancy).toEqual(5319);
-  //   expect(reuseableAgeOld.difference).toEqual(-7979);
-  // });
-
-  // test("Should determine age, expectancy, and difference between them on Mercury", () => {
-  //   reuseableAgeYoung.earthToMercury();
-  //   expect(reuseableAgeYoung.age).toEqual(104);
-  //   expect(reuseableAgeYoung.expectancy).toEqual(41667);
-  //   expect(reuseableAgeYoung.difference).toEqual(41563);
-  //   //
-  //   reuseableAgeOld.earthToMars();
-  //   expect(reuseableAgeOld.age).toEqual(13298);
-  //   expect(reuseableAgeOld.expectancy).toEqual(5319);
-  //   expect(reuseableAgeOld.difference).toEqual(-7979);
+  test("Should determine age, expectancy, and difference between them on Venus", () => {
+    reuseableAgeYoung.earthToVenus();
+    expect(reuseableAgeYoung.age).toEqual(40);
+    expect(reuseableAgeYoung.expectancy).toEqual(16129);
+    expect(reuseableAgeYoung.difference).toEqual(16089);
+    //
+    reuseableAgeOld.earthToVenus();
+    expect(reuseableAgeOld.age).toEqual(40323);
+    expect(reuseableAgeOld.expectancy).toEqual(16129);
+    expect(reuseableAgeOld.difference).toEqual(
+      "You have exceeded your lifespan by 24194 years!"
+    );
   });
 
+  test("Should determine age, expectancy, and difference between them on Jupiter", () => {
+    reuseableAgeYoung.earthToJupiter();
+    expect(reuseableAgeYoung.age).toEqual(2);
+    expect(reuseableAgeYoung.expectancy).toEqual(843);
+    expect(reuseableAgeYoung.difference).toEqual(841);
+    //
+    reuseableAgeOld.earthToJupiter();
+    expect(reuseableAgeOld.age).toEqual(2108);
+    expect(reuseableAgeOld.expectancy).toEqual(843);
+    expect(reuseableAgeOld.difference).toEqual(
+      "You have exceeded your lifespan by 1265 years!"
+    );
+  });
+
+  test("Should determine age, expectancy, and difference between them on Mercury", () => {
+    reuseableAgeYoung.earthToMercury();
+    expect(reuseableAgeYoung.age).toEqual(104);
+    expect(reuseableAgeYoung.expectancy).toEqual(41667);
+    expect(reuseableAgeYoung.difference).toEqual(41563);
+    //
+    reuseableAgeOld.earthToMercury();
+    expect(reuseableAgeOld.age).toEqual(104167);
+    expect(reuseableAgeOld.expectancy).toEqual(41667);
+    expect(reuseableAgeOld.difference).toEqual("You have exceeded your lifespan by 62500 years!");
+  });
+});
--- src/businesslogic.js
+++ src/businesslogic.js
@@ -18,18 +18,30 @@ export class Age {
   earthToVenus() {
     this.age = (Math.round(this.age / 0.62));
     this.expectancy = (Math.round(this.expectancy / 0.62));
-    this.difference = this.expectancy - this.age;
+    if ((this.expectancy - this.age) < 1) {
+      this.difference = "You have exceeded your lifespan by " + Math.abs(this.expectancy - this.age) + " years!"
+    } else {
+      this.difference = this.expectancy - this.age;
+    }
   }
 
   earthToJupiter() {
     this.age = (Math.round(this.age / 11.86));
     this.expectancy = (Math.round(this.expectancy / 11.86));
-    this.difference = this.expectancy - this.age;
+    if ((this.expectancy - this.age) < 1) {
+      this.difference = "You have exceeded your lifespan by " + Math.abs(this.expectancy - this.age) + " years!"
+    } else {
+      this.difference = this.expectancy - this.age;
+    }
   }
 
   earthToMercury() {
     this.age = (Math.round(this.age / 0.24));
     this.expectancy = (Math.round(this.expectancy / 0.24));
-    this.difference = this.expectancy - this.age;
+    if ((this.expectancy - this.age) < 1) {
+      this.difference = "You have exceeded your lifespan by " + Math.abs(this.expectancy - this.age) + " years!"
+    } else {
+      this.difference = this.expectancy - this.age;
+    }
   }
 }
